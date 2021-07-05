6c08b3d36bbdb792b6e33410e374d07ef480a166
Iftakher99 <19tonmoy99@gmail.com>
2021-07-05 21:46:44 +0600

    added image carasol

--- js/index.js
+++ js/index.js
@@ -1,25 +1,3 @@
-// $(document).ready(function () {
-//   $(".owl-carousel").owlCarousel();
-// });
-// var owl = $(".owl-carousel");
-// owl.owlCarousel({
-//   items: 8,
-//   loop: true,
-//   margin: 5,
-//   autoplay: true,
-//   // preventActionWhenRunning: true,
-//   autoplayTimeout: 900,
-//   autoplayHoverPause: true,
-//   speed: 750,
-//   mouseDrag: true,
-// });
-// $(".play").on("click", function () {
-//   owl.trigger("play.owl.autoplay", [1000]);
-// });
-// $(".stop").on("click", function () {
-//   owl.trigger("stop.owl.autoplay");
-// });
-// marquee
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
   '--marquee-elements-displayed',
